5059693ed815efb01e019727a04c41332b466d33
Mamun <bizzmamun@gmail.com>
2020-05-31 20:07:49 +0600

    fun

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
 
 void main() {
-  runApp(MaterialApp(
-    routes: {
-      '/': (context) => Loading(),
-      '/home': (context) => Home(),
-      '/location': (context) => ChooseLocation()
-    },
-  ));
+  runApp(
+    MaterialApp(
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation()
+      },
+    ),
+  );
 }
