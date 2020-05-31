73e6d129411af3c663edd0afba2d723c3b95ff4d
Mamun <bizzmamun@gmail.com>
2020-05-31 15:30:22 +0600

    just use the world time api , hurray

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,17 +9,27 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
+  void getTime() async {
     Response response =
-        await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map body = jsonDecode(response.body);
-    print(body);
+        await get('https://worldtimeapi.org/api/timezone/Asia/Dhaka');
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    String dateTime = data['utc_datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    // print(dateTime);
+    // print(offset);
+
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
