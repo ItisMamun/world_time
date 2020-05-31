273071e8e26c632c489938a3ed86b3167c49d3b8
Mamun <bizzmamun@gmail.com>
2020-05-31 14:55:06 +0600

    messed the whole thing :)

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,16 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   @override
   _LoadingState createState() => _LoadingState();
 }
 
-void getData() async {}
-
 class _LoadingState extends State<Loading> {
+  void getData() async {
+    Response response =
+        await get('https://jsonplaceholder.typicode.com/todos/1');
+    Map body = jsonDecode(response.body);
+    print(body);
+  }
+
   @override
   void initState() {
     super.initState();
+    getData();
   }
 
   @override
